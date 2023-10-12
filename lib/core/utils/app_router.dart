e61764e9a78009bb0338919926b884b664009f46
OmarIssa0 <omar.issa.slwade@gmail.com>
2023-10-12 17:27:38 +0300

    create views app and nav bar

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,13 +1,38 @@
 import 'package:go_router/go_router.dart';
-import 'package:new_shope/features/root_view/presentation/view/root_view.dart';
+import 'package:new_shope/features/cart/presentation/view/cart_view.dart';
+import 'package:new_shope/features/home/presentation/view/home_view.dart';
+import 'package:new_shope/features/profile/presentation/view/profile_view.dart';
+import 'package:new_shope/features/search/presentation/view/search_view.dart';
+import 'package:new_shope/root_view.dart';
 
 class AppRouter {
+  static const kHome = '/kHomeView';
+  static const kSearch = '/kSearchView';
+  static const kCart = '/kCartView';
+  static const kProfile = '/kProfileView';
+
   static final router = GoRouter(
     routes: [
       GoRoute(
         path: '/',
         builder: (context, state) => const RootView(),
-      )
+      ),
+      GoRoute(
+        path: kHome,
+        builder: (context, state) => const HomeView(),
+      ),
+      GoRoute(
+        path: kSearch,
+        builder: (context, state) => const SearchView(),
+      ),
+      GoRoute(
+        path: kCart,
+        builder: (context, state) => const CartView(),
+      ),
+      GoRoute(
+        path: kProfile,
+        builder: (context, state) => const ProfileView(),
+      ),
     ],
   );
 }
---
+++ lib/root_view.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:iconly/iconly.dart';
+import 'package:new_shope/features/cart/presentation/view/cart_view.dart';
+import 'package:new_shope/features/home/presentation/view/home_view.dart';
+import 'package:new_shope/features/profile/presentation/view/profile_view.dart';
+import 'package:new_shope/features/search/presentation/view/search_view.dart';
+
+class RootView extends StatefulWidget {
+  const RootView({super.key});
+
+  @override
+  State<RootView> createState() => _RootViewState();
+}
+
+class _RootViewState extends State<RootView> {
+  late PageController controller;
+  int currentScreen = 0;
+  List<Widget> screen = [
+    const HomeView(),
+    const SearchView(),
+    const CartView(),
+    const ProfileView(),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    controller = PageController(
+      initialPage: currentScreen,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBody: true,
+      resizeToAvoidBottomInset: false,
+      backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+      bottomNavigationBar: SafeArea(
+        bottom: true,
+        child: Padding(
+          padding: EdgeInsets.all(25.0.r),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(25.r),
+            child: GNav(
+              iconSize: 24.w,
+              onTabChange: (index) {
+                setState(() {
+                  currentScreen = index;
+                });
+                controller.jumpToPage(currentScreen);
+              },
+              activeColor: Colors.white,
+              backgroundColor: const Color(0xff121212).withOpacity(.75),
+              color: const Color(0xff888888).withOpacity(1),
+              tabs: const [
+                GButton(
+                  icon: IconlyBold.home,
+                ),
+                GButton(
+                  icon: IconlyBold.search,
+                ),
+                GButton(
+                  icon: IconlyBold.bag_2,
+                ),
+                GButton(
+                  icon: IconlyBold.profile,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      body: PageView(
+        physics: const NeverScrollableScrollPhysics(),
+        controller: controller,
+        children: screen,
+      ),
+    );
+  }
+}
